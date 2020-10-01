4699ceb050a86c18c97c97e2303127741fd63455
Mohanee <mohanee99@gmail.com>
2020-10-01 18:27:05 +0530

    [mohanee] refactor edited master

--- AddressBookProblem/ABook.cs
+++ AddressBookProblem/ABook.cs
@@ -7,7 +7,6 @@ namespace AddressBookProblem
     class ABook
     {
         List<Contact> aBook = new List<Contact>();
-       // List<Contact>.Enumerator em = aBook.GetEnumerator();
 
         public void setAddBook(List<Contact> addBook)
         {
@@ -27,13 +26,71 @@ namespace AddressBookProblem
         {
             foreach (Contact c in aBook)
             {
-                Console.WriteLine("\n");
-                Console.WriteLine("First Name : " + c.getFirstName());
-                Console.WriteLine("Last Name : " + c.getLastName());
-                Console.WriteLine("Email : " + c.getEmail());
-                Console.WriteLine("Address : " + c.getAddress());
-                Console.WriteLine("Phone Number : " + c.getPhone());
+                displayContact(c);
             }
+        }
+
+        public Contact SearchUsingName(string fname, string lname)
+        {
+            Contact cnew = null;
+            foreach(Contact c in aBook)
+            {
+                if(c.getFirstName().Equals(fname) && c.getLastName().Equals(lname))
+                {
+                    cnew = c;
+                    break;
+                }
+            }
+            return cnew;
+        }
+        
+        public Contact editContact(Contact c, int k)
+        {
+            switch (k)
+            {
+                case 1:
+                    Console.WriteLine("Enter the new First name");
+                    string fname = Console.ReadLine();
+                    c.setFirstName(fname);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter the new Last name");
+                    string lname = Console.ReadLine();
+                    c.setLastName(lname);
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter the new Email");
+                    string email = Console.ReadLine();
+                    c.setEmail(email);
+                    break;
+
+                case 4:
+                    Console.WriteLine("Enter the new Address");
+                    string add = Console.ReadLine();
+                    c.setAddress(add);
+                    break;
+
+                case 5:
+                    Console.WriteLine("Enter the new phone number");
+                    long pNum = long.Parse(Console.ReadLine());
+                    c.setPhone(pNum);
+                    break;
+
+                default: break;
+            }
+
+            return (c);
+        }
+
+        public void displayContact(Contact c)
+        {
+            Console.WriteLine("First Name : " + c.getFirstName());
+            Console.WriteLine("Last Name : " + c.getLastName());
+            Console.WriteLine("Email : " + c.getEmail());
+            Console.WriteLine("Address : " + c.getAddress());
+            Console.WriteLine("Phone Number : " + c.getPhone());
         }
 
     }
