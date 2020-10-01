0cde0028c4a2c3b3371c21568baf60fec7baef8d
Mohanee <mohanee99@gmail.com>
2020-10-01 11:07:04 +0530

    [mohanee] refactor addNewContact code modified

--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -10,7 +10,7 @@ namespace AddressBookProblem
             Console.WriteLine("Enter the details of the contact to be added sepetated by space");
             string alldata = Console.ReadLine();
             string[] sepData = alldata.Split(" ");
-            Contact c1 = new Contact(sepData[0],sepData[1],sepData[2],sepData[3],sepData[4]);
+            Contact c1 = new Contact(sepData[0],sepData[1],sepData[2],sepData[3],long.Parse(sepData[4]));
             ABook a = new ABook();
             a.addContact(c1);
             Console.WriteLine("Contact successfully added \n Following are the details");
